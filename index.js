ef56b1aa25bb4af0d543f38a1563b5d80bf3603a
Muzahidul-Islam-Zehad <zehadkazi88@gmail.com>
2025-01-19 18:49:36 +0600

    subscription api added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const Stripe = require('stripe');
 
 app.use(cors({
     origin: ['http://localhost:5173', 'https://newsdaylight-99199.web.app', 'https://newsdaylight-99199.firebaseapp.com']
@@ -11,6 +12,7 @@ app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
+const stripe = Stripe(`${process.env.Stripe_Secret}`);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j876r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -73,21 +75,21 @@ async function run() {
             res.send('user already found');
         });
         //get all user data
-        app.get('/all-users', async(req,res)=>{
+        app.get('/all-users', async (req, res) => {
             const result = await usersCollection.find().toArray();
 
             res.send(result);
         })
 
         //update user to admin
-        app.patch('/make-admin/:id', async(req, res)=>{
+        app.patch('/make-admin/:id', async (req, res) => {
             const id = req.params.id;
             const query = {
-                _id : new ObjectId(id),
+                _id: new ObjectId(id),
             }
             const updatedDoc = {
-                $set : {
-                    role : 'Admin'
+                $set: {
+                    role: 'Admin'
                 }
             }
 
@@ -96,44 +98,101 @@ async function run() {
         })
 
         //get user profile
-        app.get('/users/:id', async(req,res)=>{
+        app.get('/users/:id', async (req, res) => {
             const email = req.params.id;
 
-            const query = {email};
+            const query = { email };
 
             const result = await usersCollection.findOne(query);
             res.send(result);
         })
 
         //get users count
-        app.get('/users-count', async(req, res)=>{
+        app.get('/users-count', async (req, res) => {
             const allUserCount = await usersCollection.estimatedDocumentCount();
-            const premiumUsersCount = await usersCollection.countDocuments({premiumTaken : 'Yes'});
-            const normalUsersCount = await usersCollection.countDocuments({premiumTaken : 'No'});
-            
+            const premiumUsersCount = await usersCollection.countDocuments({ premiumTaken: 'Yes' });
+            const normalUsersCount = await usersCollection.countDocuments({ premiumTaken: 'No' });
+
             res.send({
                 allUserCount, normalUsersCount, premiumUsersCount
             })
-        } )
+        })
 
         //Update user profile
-        app.patch('/users/:id', async(req, res)=>{
+        app.patch('/users/:id', async (req, res) => {
             const email = req.params.id;
 
             const userData = req.body;
 
-            const query = {email};
+            const query = { email };
             const updatedDoc = {
-                $set : {
-                    name : userData.name,
-                    photoURL : userData.photoURL
+                $set: {
+                    name: userData.name,
+                    photoURL: userData.photoURL
                 }
             }
 
             const result = await usersCollection.updateOne(query, updatedDoc);
             res.send(result);
-            
-        } )
+
+        })
+
+        //make user normal to premium
+        app.patch('/make-premium-user', async (req, res) => {
+            const { email, time } = req.body;
+
+            const query = { email }
+
+            const now = new Date();
+
+            const updatedDoc = {
+                $set: {
+                    premiumEndAt: new Date(now.getTime() + time * 1000)
+                }
+            }
+
+            const result = await usersCollection.updateOne(query, updatedDoc);
+
+            res.send(result);
+
+            // console.log( now,updatedDoc);
+        })
+
+        //Check isSubscribed?
+        app.get('/isPremium', async (req, res) => {
+
+            const email = req.query.email;
+
+            const query = { email };
+
+            let isSubscribed = false;
+
+            const result = await usersCollection.findOne(query);
+            if (result?.premiumEndAt) {
+                const premiumEndAt = new Date(result.premiumEndAt);
+                const now = new Date();
+                if (premiumEndAt > now) {
+
+                    isSubscribed = true;
+                }
+            }
+
+            res.send(isSubscribed);
+        })
+
+        //remove subscription of the user
+        app.patch('/remove/subscription', async(req,res)=>{
+            const email = req.body;
+            const query = {email};
+            const updatedDoc = {
+                $set : {
+                    premiumEndAt : null,
+                }
+            }
+
+            const result = await usersCollection.updateOne(query, updatedDoc);
+            res.send(result);
+        })
 
         //add articles in articles collection
         app.post('/articles', async (req, res) => {
@@ -148,8 +207,8 @@ async function run() {
                 userInfo: articleData.userInfo,
                 status: 'Pending',
                 isPremium: 'No',
-                totalViewCount : 0,
-                createdAt : new Date()
+                totalViewCount: 0,
+                createdAt: new Date()
             }
 
             const result = await articlesCollection.insertOne(updatedArticleData);
@@ -158,31 +217,31 @@ async function run() {
         })
 
         //get all article data to admin
-        app.get('/all-articles/data', async(req,res)=>{
+        app.get('/all-articles/data', async (req, res) => {
             const result = await articlesCollection.find().toArray();
             res.send(result);
         })
 
         //get trending article data
-        app.get('/articles/trending', async(req, res)=>{
-            const query = {status : 'Approved'};
-            const result = await articlesCollection.find(query).sort({totalViewCount : -1}).limit(6).toArray();
+        app.get('/articles/trending', async (req, res) => {
+            const query = { status: 'Approved' };
+            const result = await articlesCollection.find(query).sort({ totalViewCount: -1 }).limit(6).toArray();
 
             res.send(result);
         })
 
         //update view count
-        app.patch('/articles/view-count/:id', async(req,res)=>{
+        app.patch('/articles/view-count/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id : new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const updatedDoc = {
-                $inc : {
-                    totalViewCount : 1
+                $inc: {
+                    totalViewCount: 1
                 }
             }
 
             const result = await articlesCollection.updateOne(query, updatedDoc)
-            res.send(result )
+            res.send(result)
         })
 
         // update article based on article id
@@ -205,12 +264,12 @@ async function run() {
         })
 
         //update article status approve
-        app.patch('/update/status/:id', async(req, res)=>{
+        app.patch('/update/status/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id : new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const updatedDoc = {
-                $set : {
-                    status : 'Approved'
+                $set: {
+                    status: 'Approved'
                 }
             }
 
@@ -220,33 +279,32 @@ async function run() {
         })
 
         //article decline
-        app.patch('/article/decline/:id', async(req, res)=>
-        {
+        app.patch('/article/decline/:id', async (req, res) => {
             const id = req.params.id;
             const reason = req.body;
 
-            const query = {_id : new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
 
             const updatedDoc = {
-                $set : {
-                    status : 'Declined',
-                    decliningReason : reason.decliningReason
+                $set: {
+                    status: 'Declined',
+                    decliningReason: reason.decliningReason
                 }
             }
-            
+
             const result = await articlesCollection.updateOne(query, updatedDoc);
 
             res.send(result);
         })
 
         //make premium article
-        app.patch('/make-premium/:id',async(req, res)=>{
+        app.patch('/make-premium/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id : new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const updatedDoc = {
-                $set : 
+                $set:
                 {
-                    isPremium : "Yes"
+                    isPremium: "Yes"
                 }
             }
 
@@ -286,23 +344,23 @@ async function run() {
         });
 
         //Add publisher
-        app.post('/add-publishers', async(req, res)=>{
+        app.post('/add-publishers', async (req, res) => {
             const publisherData = req.body;
             const result = await publishersCollection.insertOne(publisherData);
             res.send(result);
         })
 
         // get all publisher data
-        app.get('/publisher-data', async(req, res)=>{
+        app.get('/publisher-data', async (req, res) => {
             const result = await publishersCollection.find().toArray();
             res.send(result);
         })
 
         //get all premium articles
-        app.get('/premium/articles/only', async(req, res)=>{
+        app.get('/premium/articles/only', async (req, res) => {
             const query = {
-                status : 'Approved',
-                isPremium : 'Yes'
+                status: 'Approved',
+                isPremium: 'Yes'
             };
             const result = await articlesCollection.find(query).toArray();
 
@@ -310,7 +368,7 @@ async function run() {
         })
 
         //get all approved articles
-        app.get('/all-articles/approved', async(req,res)=>{
+        app.get('/all-articles/approved', async (req, res) => {
 
             const search = req.query.search;
             const publicationFiler = req.query.publicationFiler;
@@ -319,32 +377,44 @@ async function run() {
             // console.log(search, publicationFiler, tags);
 
             let query = {
-                status : 'Approved'
+                status: 'Approved'
             }
 
-            if(search)
-            {
+            if (search) {
                 query.articleTitle = {
-                    $regex : search,
-                    $options : 'i'
+                    $regex: search,
+                    $options: 'i'
                 };
             }
 
-            if(publicationFiler)
-            {
+            if (publicationFiler) {
                 query.publisher = publicationFiler
             }
 
-            if(tags)
-            {
+            if (tags) {
                 const tagsArray = tags.split(',');
                 query.Tags = {
-                    $all : tagsArray
+                    $all: tagsArray
                 };
             }
 
             const result = await articlesCollection.find(query).toArray();
-            res.send(result);  
+            res.send(result);
+        })
+
+        // create payment intent
+        app.post('/create-payment-intent', async (req, res) => {
+            const { amount } = req.body;
+            const price = parseInt(amount) * 100;
+            // console.log(price);
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: price,
+                currency: 'usd'
+            })
+
+            res.send({ clientSecret: paymentIntent.client_secret })
+
         })
 
     } finally {
